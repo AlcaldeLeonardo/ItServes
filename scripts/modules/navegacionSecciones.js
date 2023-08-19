ce7959067d1284704090078e2e3d98bf6d46c88c
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2023-08-19 19:41:47 -0300

    navegacion mejorada
    se muestran u ocultan las diferentes secciones con el navBar

--- scripts/modules/navegacionSecciones.js
+++ scripts/modules/navegacionSecciones.js
@@ -1,33 +1,41 @@
-export function navegar(){
+export function navegar() {
     const $btnTuItServes = document.getElementById("btnTuItServes");
-const $btnHorarios = document.getElementById("btnHorarios");
+    const $btnHorarios = document.getElementById("btnHorarios");
 
-const $seccionHorarios = document.getElementById("seccionHorarios");
-const $seccionItServes = document.getElementById("seccionItServes");
+    const $seccionHorarios = document.getElementById("seccionHorarios");
+    const $seccionItServes = document.getElementById("seccionItServes");
+    const $seccionInfo = document.getElementById("seccionInfo");
+    const $seccionBanner = document.getElementById("seccionBanner");
 
-$btnTuItServes.addEventListener("click",()=> {
-    if($seccionHorarios.style.display != "none")
-        $seccionHorarios.style.display = "none";
+    $btnTuItServes.addEventListener("click", () => {
+        if ($seccionHorarios.style.display != "none") $seccionHorarios.style.display = "none";
+        if ($seccionInfo.style.display != "none") $seccionInfo.style.display = "none";
+        if ($seccionBanner.style.display != "none") $seccionBanner.style.display = "none";
+        
+        
+        $seccionItServes.style.display = "block";
 
-    $seccionItServes.style.display = "block";
+        $btnHorarios.removeAttribute("aria-current");
+        $btnHorarios.classList.remove("active");
 
-    $btnHorarios.removeAttribute("aria-current");
-    $btnHorarios.classList.remove("active");
+        $btnTuItServes.setAttribute("aria-current", "page");
+        $btnTuItServes.classList.add("active");
+    });
 
-    $btnTuItServes.setAttribute("aria-current","page");
-    $btnTuItServes.classList.add("active");
-});
+    $btnHorarios.addEventListener("click", () => {
+        if ($seccionItServes.style.display != "none") $seccionItServes.style.display = "none";
+        if ($seccionBanner.style.display != "none") $seccionBanner.style.display = "none";
+        if ($seccionInfo.style.display != "none") $seccionInfo.style.display = "none";
 
-$btnHorarios.addEventListener("click", ()=> {
-    if($seccionItServes.style.display != "none")
-        $seccionItServes.style.display = "none";
+        $seccionHorarios.style.display = "block";
 
-    $seccionHorarios.style.display = "block";
+        $seccionHorarios.classList.add("mt-5");
+        $seccionHorarios.classList.add("pt-5");
 
-    $btnTuItServes.removeAttribute("aria-current");
-    $btnTuItServes.classList.remove("active");
+        $btnTuItServes.removeAttribute("aria-current");
+        $btnTuItServes.classList.remove("active");
 
-    $btnHorarios.setAttribute("aria-current","page");
-    $btnHorarios.classList.add("active");
-})
+        $btnHorarios.setAttribute("aria-current", "page");
+        $btnHorarios.classList.add("active");
+    });
 }
